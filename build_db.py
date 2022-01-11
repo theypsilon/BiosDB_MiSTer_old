ec9f9021feb1a8bbb9673f63de52216aa62c1163
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-01-11 02:33:37 +0100

    New BIOS system.

--- build_db.py
+++ build_db.py
@@ -17,22 +17,13 @@
 # You can download the latest version of this tool from:
 # https://github.com/MiSTer-unstable-nightlies/Unstable_Folder_MiSTer
 
-import hashlib
 import json
 import time
-import os
-import sys
 
 def main():
     print('START!')
 
-    local_files_path = (sys.argv[1] + "/") if len(sys.argv) == 3 else None
-    base_files_url = (sys.argv[2] + "/") if len(sys.argv) == 3 else None
-
-    if not local_files_path or not base_files_url:
-        print('Wrong arguments!')
-        print('Usage: %s local_files_path base_files_url' % sys.argv[0])
-        exit(-1)
+    base_files_url = 'https://archive.org/download/mister-console-bios-pack_theypsilon'
 
     db = {
         "db_id": 'bios_db',
@@ -45,33 +36,22 @@ def main():
         "timestamp":  int(time.time())
     }
 
-    for system, roms in load_json('bios.json').items():
+    for system, roms in load_json('bios_definitions.json').items():
         gamesdir = "games/" + system
         db["folders"][gamesdir] = {}
-        for mister_rom, remote_rom in roms.items():
-            rom_path = system + "/" + remote_rom
-            local_rom_path = local_files_path + rom_path
-            rom_url = base_files_url + rom_path
+        zip = system
+        if 'zip' in roms:
+            zip = roms['zip']
+            roms.pop('zip')
+        for mister_rom, description in roms.items():
             db['files'][gamesdir + '/' + mister_rom] = {
-                "hash": hash(local_rom_path),
-                "size": size(local_rom_path),
-                "url": rom_url,
+                "hash": description['hash'],
+                "size": description['size'],
+                "url": '%s/%s.zip/%s' % (base_files_url, zip, description['file']),
                 "overwrite": False
             }
 
     save_json(db, "bios_db.json")
-
-def hash(file):
-    with open(file, "rb") as f:
-        file_hash = hashlib.md5()
-        chunk = f.read(8192)
-        while chunk:
-            file_hash.update(chunk)
-            chunk = f.read(8192)
-        return file_hash.hexdigest()
-
-def size(file):
-    return os.path.getsize(file)
 
 def save_json(db, json_name):
     with open(json_name, 'w') as f:
